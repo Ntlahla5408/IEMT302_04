90a653eec57c735f5c151c31f865e17f5bb85ef1
Ntlahla5408 <s226083470@Mandela.ac.za>
2026-07-29 21:49:18 +0200

    Add Beautiful Soup web scraping example

--- scrape.py
+++ scrape.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
+
 import requests
 from bs4 import BeautifulSoup
 
-if __name__ == ['\"]__main__['\"]:
+
+def main():
+    url = "https://www.sanews.gov.za/"
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    print("Latest South African News Headlines:\n")
+
+    for heading in soup.find_all("h2")[:5]:
+        print("-", heading.get_text(strip=True))
+
+
+if __name__ == "__main__":
+    main()
